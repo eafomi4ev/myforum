69cdb0b8bd681f7da80b430b3f586aad7d7297f5
eafomi4ev <eafomi4ev@gmail.com>
2017-03-05 13:08:02 +0300

    Удалил лишнее для настрйоки мапинга

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -2,16 +2,9 @@ package controllers;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import services.UserDAO;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 //import com.google.gson.Gson;
 //import com.google.gson.GsonBuilder;
@@ -21,7 +14,7 @@ import java.sql.SQLException;
 //import start.UserProfile;
 
 /**
- * Created by egor on 23.02.17.
+ * Created by egor on 03.03.17.
  */
 
 @RestController
@@ -34,33 +27,10 @@ public class UserController {
     }
 
 
-    @RequestMapping(path = "/signup", method = RequestMethod.POST, produces = "application.properties/json", consumes = "application/json")
-    public ResponseEntity signup() {
+    @RequestMapping(path = "/test")
+    public String test() {
 
-        Connection connection = null;
-        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
-        String url = "jdbc:postgresql://localhost:5000/myforum";
-        //Имя пользователя БД
-        String name = "egor";
-        //Пароль
-        String password = "";
-
-        try {
-            //Загружаем драйвер
-            Class.forName("org.postgresql.Driver");
-            System.out.println("Драйвер подключен");
-            //Создаём соединение
-            connection = DriverManager.getConnection(url, name, password);
-            System.out.println("Соединение установлено");
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new JSONObject().put("msg", "Тест пройден").toString());
+        return new JSONObject().put("msg", "Связь есть").toString();
 
     }
 
